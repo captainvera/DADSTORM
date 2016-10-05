ac89da86a67436d38e8f89081ee1b5cfeaf6ae3a
Jorge Pessoa <jpessoa.on@gmail.com>
2016-10-05 19:49:56 +0100

    Added generic interface & primary implementation of routing strategies

---
+++ Replica/RoutingStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DADSTORM
+{
+    interface IRoutingStrategy
+    {
+        void setReplicaList(List<string> replicas);
+        void send(String data);
+    }
+
+    class PrimaryRoutingStrategy : IRoutingStrategy
+    {
+        
+        public void setReplicaList(List<string> replicas)
+        {
+
+        }
+
+        public void send(String data)
+        {
+        
+        }
+    }
+}
