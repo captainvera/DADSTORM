f40673387bf39343b095443b3e5987e106f6adb9
Jose-Francisco <jose.francisco.semedo@gmail.com>
2016-10-14 18:15:28 +0100

    Moved parser to its own class. DTOs now stored in dictionary.

---
+++ Puppetmaster/Parser.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace DADSTORM {
+    class Parser {
+
+        public Dictionary<string, OperatorDTO> makeOperatorDTOs(string[] splitFile) {
+
+            System.Console.WriteLine("Building Operator drafts from previously split file.");
+
+            //ArrayList operatorDTOs = new ArrayList();
+            Dictionary<string, OperatorDTO> operatorDTOs = new Dictionary<string, OperatorDTO>();
+
+            string id = "placeholder";
+            string rep = "placeholder";
+            string rout = "placeholder";
+            ArrayList inputs = new ArrayList();
+            ArrayList addr = new ArrayList();
+            ArrayList spec = new ArrayList();
+
+            int n, i;
+            n = i = 0;
+
+            while (n < splitFile.Length) {
+                System.Console.WriteLine("\nWord being filtered: {0} - n = {1}", splitFile[n], n);
+                switch (splitFile[n]) {
+                    case "INPUT_OPS":
+                        i = n + 1;
+                        while (!splitFile[i].Equals("REP_FACT")) {
+                            System.Console.WriteLine("Operator's input: {0}", splitFile[i]);
+                            inputs.Add(splitFile[i]);
+                            i++;
+                        }
+                        n = i - 1;
+                        break;
+                    case "REP_FACT":
+                        rep = splitFile[n + 1];
+                        System.Console.WriteLine("Operator rep factor: {0}", rep);
+                        n++;
+                        break;
+                    case "ROUTING":
+                        rout = splitFile[n + 1];
+                        System.Console.WriteLine("Operator routing policy: {0}", rout);
+                        n++;
+                        break;
+                    case "ADDRESS":
+                        i = n + 1;
+                        while (!splitFile[i].Equals("OPERATOR_SPEC")) {
+                            System.Console.WriteLine("Adresses: {0}", splitFile[i]);
+                            addr.Add(splitFile[i]);
+                            i++;
+                        }
+                        n = i - 1;
+                        break;
+                    case "OPERATOR_SPEC":
+                        i = n + 1;
+                        while (!splitFile[i].Equals("INPUT_OPS")) {
+                            System.Console.WriteLine("Operator spec items: {0}. i = {1}", splitFile[i], i);
+                            spec.Add(splitFile[i]);
+                            i++;
+                            if (i == splitFile.Count()) {
+                                break;
+                            }
+                        }
+                        if (i < splitFile.Count()) {
+                            spec.RemoveAt(spec.Count - 1);
+                            System.Console.WriteLine("spec array's current last item: {0}", spec[spec.Count - 1]);
+                            n = i - 2;
+                        }
+                        else {
+                            n = i;
+                        }
+                        operatorDTOs.Add(id, new OperatorDTO(id, inputs, rep, rout, addr, spec));
+                        System.Console.WriteLine("Added new operator draft to ArrayList. Continue?");
+                        inputs = new ArrayList();
+                        addr = new ArrayList();
+                        spec = new ArrayList();
+                        System.Console.ReadLine();
+                        break;
+                    default:
+                        id = splitFile[n];
+                        System.Console.WriteLine("New operator with id: {0}", id);
+                        System.Console.WriteLine("Entered default statement.");
+                        break;
+                }
+                n++;
+            }
+            System.Console.WriteLine("Drafts all done.");
+            return operatorDTOs;
+        }
+
+        public string[] readConfig() {
+            //read file as one string
+            string config_file = System.IO.File.ReadAllText(@"C:\Users\José Semedo\Desktop\DAD\DADSTORM\CONFIG_FILE");
+
+            //TODO remove - print string read   --- ask for input
+            System.Console.WriteLine("Contents of CONFIG_FILE: {0}", config_file);
+
+            //splitting text
+            char[] splitChars = { ' ', ',', '\t', '\n' };
+            string[] splitFile = config_file.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string st in splitFile) {
+                System.Console.WriteLine(st);
+            }
+
+            System.Console.WriteLine("Done splitting. Size = {0}. Continue?", splitFile.Count());
+            System.Console.ReadLine();
+
+            return splitFile;
+        }
+    }
+
+    class OperatorDTO {
+        public string op_id;
+        public ArrayList input_ops = new ArrayList();
+        public string rep_fact;
+        public string routing;
+        public ArrayList address = new ArrayList();
+        public ArrayList op_spec = new ArrayList();
+
+        public OperatorDTO(string id, ArrayList inputs, string rep, string rout, ArrayList addr, ArrayList spec) {
+            op_id = id;
+            input_ops = inputs;
+            rep_fact = rep;
+            routing = rout;
+            address = addr;
+            op_spec = spec;
+        }
+    }
+
+}
--- Puppetmaster/Program.cs
+++ Puppetmaster/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace DADSTORM
 {
@@ -15,11 +16,14 @@ namespace DADSTORM
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Starting Puppetmaster");
+            Parser parser = new Parser();
+            Dictionary<string, OperatorDTO> operatorDTOs = parser.makeOperatorDTOs(parser.readConfig());
             Puppetmaster pm = new Puppetmaster();
+
             log = new Logger("Puppetmaster");
 
-            pm.makeOperatorDrafts(pm.readConfig());
+            //pm.makeOperatorDrafts(pm.readConfig());
 
             /*
             TcpChannel channel = new TcpChannel();
@@ -41,131 +45,9 @@ namespace DADSTORM
         }
     }
     class Puppetmaster{
-
-        public string[] readConfig()
-        {
-            //read file as one string
-            string config_file = System.IO.File.ReadAllText(@"C:\Users\José Semedo\Desktop\DAD\DADSTORM\CONFIG_FILE");
-
-            //TODO remove - print string read   --- ask for input
-            System.Console.WriteLine("Contents of CONFIG_FILE: {0}", config_file);
-
-            //splitting text
-            char[] splitChars = {' ', ',', '\t', '\n'};
-            string[] splitFile= config_file.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string st in splitFile){
-                System.Console.WriteLine(st);
-            }
-
-            System.Console.WriteLine("Done splitting. Size = {0}. Continue?", splitFile.Count());
-            System.Console.ReadLine();
-
-            return splitFile;
-        }
         
-        public ArrayList makeOperatorDrafts(string[] splitFile){
-
-            System.Console.WriteLine("Building Operator drafts from previously split file.");
-
-            ArrayList operatorDrafts = new ArrayList();
-
-            string id = "placeholder";
-            int rep = 20; //should never be 20
-            string rout = "placeholder";
-            ArrayList inputs = new ArrayList();
-            ArrayList addr = new ArrayList();
-            ArrayList spec = new ArrayList();
-
-            int n, i;
-            n = i = 0;
-
-            while(n < splitFile.Length){
-                System.Console.WriteLine("\nWord being filtered: {0} - n = {1}", splitFile[n], n);
-                switch (splitFile[n])
-                {
-                    case "INPUT_OPS":
-                        i = n+1;
-                        while (!splitFile[i].Equals("REP_FACT")){
-                            System.Console.WriteLine("Operator's input: {0}", splitFile[i]);
-                            inputs.Add(splitFile[i]);
-                            i++;
-                        }
-                        n = i - 1;
-                        break;
-                    case "REP_FACT":
-                        rep = Convert.ToInt32(splitFile[n + 1]);
-                        System.Console.WriteLine("Operator rep factor: {0}", rep);
-                        n++;
-                        break;
-                    case "ROUTING":
-                        rout = splitFile[n + 1];
-                        System.Console.WriteLine("Operator routing policy: {0}", rout);
-                        n++;
-                        break;
-                    case "ADDRESS":
-                        i = n + 1;
-                        while (!splitFile[i].Equals("OPERATOR_SPEC")) {
-                            System.Console.WriteLine("Adresses: {0}", splitFile[i]);
-                            addr.Add(splitFile[i]);
-                            i++;
-                        }
-                        n = i - 1;
-                        break;
-                    case "OPERATOR_SPEC":
-                        i = n + 1;
-                        while (!splitFile[i].Equals("INPUT_OPS")) {
-                            System.Console.WriteLine("Operator spec items: {0}. i = {1}", splitFile[i], i);
-                            spec.Add(splitFile[i]);
-                            i++;
-                            if (i == splitFile.Count()) {
-                                break;
-                            }
-                        }
-                        if (i < splitFile.Count()) {
-                            spec.RemoveAt(spec.Count - 1);
-                            System.Console.WriteLine("spec array's current last item: {0}", spec[spec.Count - 1]);
-                            n = i - 2;
-                        }
-                        else {
-                            n = i;
-                        }
-                        operatorDrafts.Add(new OperatorDraft(id, inputs, rep, rout, addr, spec));
-                        System.Console.WriteLine("Added new operator draft to ArrayList. Continue?");
-                        inputs = new ArrayList();
-                        addr = new ArrayList();
-                        spec = new ArrayList();
-                        System.Console.ReadLine();
-                        break;
-                    default:
-                        id = splitFile[n];
-                        System.Console.WriteLine("New operator with id: {0}", id);
-                        System.Console.WriteLine("Entered default statement.");
-                        break;
-                }
-                n++;
-            }
-            System.Console.WriteLine("Drafts all done.");
-            return operatorDrafts;
-        }
         
     }
 
-    class OperatorDraft{
-        public string op_id;
-        public ArrayList input_ops = new ArrayList();
-        public int rep_fact;
-        public string routing;
-        public ArrayList address = new ArrayList();
-        public ArrayList op_spec = new ArrayList();
 
-        public OperatorDraft(string id, ArrayList inputs, int rep, string rout, ArrayList addr,ArrayList spec){
-            op_id = id;
-            input_ops = inputs;
-            rep_fact = rep;
-            routing = rout;
-            address = addr;
-            op_spec = spec;
-        }
-    }
 }
