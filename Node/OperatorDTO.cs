ac6b12f3a934380270128b12c238f021ee86025d
captainvera <miguel.coimbra.vera@gmail.com>
2016-10-14 20:02:00 +0100

    Moved OperatorDTO to Node

---
+++ Node/OperatorDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace DADSTORM
+{
+    public class OperatorDTO
+    {
+        public string op_id;
+        public ArrayList input_ops = new ArrayList();
+        public string rep_fact;
+        public string routing;
+        public ArrayList address = new ArrayList();
+        public ArrayList op_spec = new ArrayList();
+
+        public OperatorDTO(string id, ArrayList inputs, string rep, string rout, ArrayList addr, ArrayList spec)
+        {
+            op_id = id;
+            input_ops = inputs;
+            rep_fact = rep;
+            routing = rout;
+            address = addr;
+            op_spec = spec;
+        }
+    }
+
+}
