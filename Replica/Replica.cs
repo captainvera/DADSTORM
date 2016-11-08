88b16bc196a599885026a9f4234322e892256785
Jose-Francisco <jose.francisco.semedo@gmail.com>
2016-11-08 01:02:00 +0000

    Previous error solved. new one: deserializing name it seems.

--- Replica/Replica.cs
+++ Replica/Replica.cs
@@ -20,21 +20,27 @@ namespace DADSTORM {
             }
             string dtoXml = args[0];
 
-            OperatorDTO op = Deserialize((string)dtoXml);
+            OperatorDTO op = Deserialize<OperatorDTO>(dtoXml);
 
             Logger.writeLine("Processed " + op.next_op_addresses.Count + " output replicas", "ReplicaProcess");
 
             //Might need proper implementation for naming: CHECK PROJ INSTR
             string id = op.op_id;
-            string name = "Replica" + op.op_id;
+            string name = "Replica" + id;
+            string replication = op.rep_fact;
+            string routing = op.routing;
+            string address = op.address[op.curr_rep];
+            string[] op_spec = op.op_spec.ToArray();
             string port = op.ports[op.curr_rep];
             string[] nextOperators = op.next_op_addresses.ToArray();
+            string logging = op.logging;
+            string semantics = op.semantics;
 
 
             TcpChannel channel = new TcpChannel(Int32.Parse(port));
             ChannelServices.RegisterChannel(channel, false);
 
-            Replica rep = new Replica(id, port.Replace("10", "11"), nextOperators);
+            Replica rep = new Replica(id, port.Replace("10", "11"), replication, routing, address, op_spec, logging, semantics, nextOperators);
 
             RemotingServices.Marshal(rep, "op", typeof(Replica));
             Logger.writeLine("Registered with name:" + name, "ReplicaProcess");
@@ -57,8 +63,8 @@ namespace DADSTORM {
     {
         private Logger log;
 
-        private string id, port;
-        private string[] output;
+        private string id, port, replication, routing, address, logging, semantics;
+        private string[] output, op_spec;
         private Boolean primary;
 
         private IOperator op;
@@ -66,18 +72,28 @@ namespace DADSTORM {
 
         private TcpChannel channel;
 
-        public Replica(string _id, string _port, string[] _next)
+        public Replica(string _id, string _port, string _replication, string _routing, string _address, string[] _op_spec, string _logging, string _semantics, string[] _next)
         {
             //Replica configuration
             primary = false;
             id = _id;
             port = _port;
             output = _next;
+            replication = _replication;
+            routing = _routing;
+            address = _address;
+            logging = _logging;
+            semantics = _semantics;
+            op_spec = _op_spec;
 
+            //log = new Logger("Replica" + id, logging);
             log = new Logger("Replica" + id);
 
             //Routing Strategy for this replica
+            //router = new routing(routing);
             router = new PrimaryRoutingStrategy(this);
+
+            //op = new op(op_spec);
             op = new DUP();
 
             log.writeLine("Initialized Replica " + id + " with out port " + port);
