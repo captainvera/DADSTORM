cdd5cd61b0f3268ac97b0e1ce4cb0603f9a1cb50
Jorge Pessoa <jpessoa.on@gmail.com>
2016-11-08 19:01:50 +0000

    Improved logger functionality

--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -8,30 +8,46 @@ namespace DADSTORM
 {
     public class Logger
     {
+        /**
+         * Logging level:
+         * 0 - Regular
+         * 1 - Verbose
+         * 2 - Debug
+         */
+
         private string id;
+
+        static int level = 1;
+
         public Logger(string _id)
         {
             id = _id;
         }
 
-        public void writeLine(string s)
+        public void writeLine(string s, params object[] args)
         {
-            Console.WriteLine("[" + id + "] " + s); 
+            Console.WriteLine("[" + id + "] " + String.Format(s, args)); 
         }
 
-        public void write(string s)
+        public void write(string s, params object[] args)
         {
-            Console.Write("[" + id + "] " + s); 
+            Console.Write("[" + id + "] " + String.Format(s, args)); 
         }
 
-        public static void writeLine(string s, string id)
+        public static void writeLine(string s, string id, params object[] args)
         {
-            Console.WriteLine("[" + id + "] " + s);
+            Console.WriteLine("[" + id + "] " + String.Format(s, args));
         }
 
-        public static void write(string s, string id)
+        public static void write(string s, string id, params object[] args)
         {
-            Console.Write("[" + id + "] " + s);
+            Console.Write("[" + id + "] " + String.Format(s, args));
+        }
+
+        public static void debug(string s, params object[] args)
+        {
+            if(level > 1)
+                Console.Write("[DEBUG] " + String.Format(s, args));
         }
     }
 }
