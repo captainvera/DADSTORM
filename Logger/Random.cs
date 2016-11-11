7013253c6bb785ad573bcdaa53ab13bc484d62ac
Captainvera <miguel.coimbra.vera@gmail.com>
2016-11-11 10:36:36 +0000

    Added ThreadSafe Random class

---
+++ Logger/Random.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DADSTORM
+{
+    public static class RandomGenerator
+    {
+        static int seed = Environment.TickCount;
+
+        static readonly ThreadLocal<Random> random =
+            new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed)));
+
+        public static int nextInt()
+        {
+            return random.Value.Next();
+        }
+
+        public static int nextInt(int min, int max)
+        {
+            return random.Value.Next(min, max+1);
+        }
+    }
+}
